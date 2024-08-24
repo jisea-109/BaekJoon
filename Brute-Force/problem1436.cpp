70cc9fb6379553a3a5e563f2eaedf5e5422386ab
sejin cho <sc19s2c@leeds.ac.uk>
2024-08-24 23:05:59 +0900

    finished 1 brute-force problem

--- Brute-Force/problem1436.cpp
+++ Brute-Force/problem1436.cpp
@@ -1,5 +1,5 @@
 /*https://www.acmicpc.net/problem/1436, 영화감독 숌*/
-
+/*숫자는 항상 차례대로 만들어야함. 1666,2666,3666,4666,5666,6661,6662,6663,6664,6665,6666,6667, 6668, 6669, 7666,*/
 #include <iostream>
 using namespace std;
 
@@ -7,5 +7,22 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    int num;
+    cin >> num;
+    int movieName = 665;
+    string findString;
+    for (int i = 0; i < num; i++)
+    {
+        while(1)
+        {
+            movieName++;
+            findString = to_string(movieName);
+            if (findString.find("666") != -1)
+            {
+                break;
+            }
+        }
+    }
+    cout << movieName;
     return 0;
 }
