d6c8d090d4250a00f8b352e8486fcd65d735de1b
sejin cho <sc19s2c@leeds.ac.uk>
2024-08-29 22:19:01 +0900

    finished sorting algorithm by using counting sort algorithm

--- Sorting algorithm/SortingAlgorithms.cpp
+++ Sorting algorithm/SortingAlgorithms.cpp
@@ -120,3 +120,32 @@ void heapify(int arr[], int N, int i)
         heapify(arr, N, largest);
     }
 }
+// quick sort, usually O(N * log N) but worst case is O(N^2)
+void quickSort(int *data, int start, int end) {
+  if (start >= end) return;
+  int pivot = start;  // 기준 값
+  int i = start + 1;
+  int j = end;
+
+  while (i <= j) {
+    while (data[i] <= data[pivot])  // 키 값보다 큰 값 만날때까지 오른쪽으로 이동
+      i++;
+    while (data[j] >= data[pivot] && j > start)  // 키 값보다 작은 값 만날 때까지 왼쪽으로 이동
+      j--;
+    if (i > j)  //현재 엇갈린 상태면 pivot 값 교체
+    {
+      int temp = data[j];
+      data[j] = data[pivot];
+      data[pivot] = temp;
+    } 
+    else 
+    {
+      int temp = data[j];
+      data[j] = data[i];
+      data[i] = temp;
+    }
+    // 재귀 호출
+    quickSort(data, start, j - 1);
+    quickSort(data, j + 1, end);
+  }
+}
--- Sorting algorithm/problem10989.cpp
+++ Sorting algorithm/problem10989.cpp
@@ -6,5 +6,26 @@ using namespace std;
 
 int main()
 {  
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+    int n, vec;
+    cin >> n;
+    vector<int> count(10001);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> vec;
+        count[vec]++;
+    }
+
+    for (int i = 1; i < 10001; i++)
+    {
+        for (int j = 0; j < count[i]; j++)
+        {
+            cout << i << "\n";
+        }
+    }
     return 0;
 }
